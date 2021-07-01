0e922d3e8b9fc122a595bd4e477b2c4951746c8a
daniszcode <danirbd13@hotmail.com>
2021-06-30 22:37:12 -0300

    bug resolvido

--- script.js
+++ script.js
@@ -7,14 +7,14 @@ const num = document.querySelector('input#addNumber');
 
 
 function isNumero(n) {
-    
+
     if (Number(n) >= 1 && Number(n) <= 100) { //verifica se os num estão entre 0 e 100//
-        
+
         return true
     } else {
         return false
     }
-    
+
 }
 
 function inLista(n, l) {
@@ -26,16 +26,16 @@ function inLista(n, l) {
 }
 
 function adicionar() {
-    
-    
-    
+
+
+
     if (isNumero(num.value) && !inLista(num.value, valores)) {
         valores.push(Number(num.value))
         let item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lista.appendChild(item)
         res.innerHTML = ''
-        item.addEventListener('click', function() { 
+        item.addEventListener('click', function () {
             item.remove()
         })
     } else {
@@ -44,7 +44,7 @@ function adicionar() {
     salvarItem()
     num.value = ""
     num.focus() // variavel + funcao nativa focus [apaga o valor do input] 
-    
+
 }
 
 
@@ -60,32 +60,41 @@ function finalizar() {
             if (item > maior) {
                 maior = item
             } else if (item < menor) {
-                menor =  item
+                menor = item
             }
             soma = soma + item
         })
         const tot = valores.length
         media = soma / tot
         res.innerHTML = ''
-        res.innerHTML += `<p>Ao todo você adicionou ${tot} numeros.</p>` 
+        res.innerHTML += `<p>Ao todo você adicionou ${tot} numeros.</p>`
         res.innerHTML += `<p>O maior numero é ${maior}</p>`
         res.innerHTML += `<p>O menor numero é ${menor}</p>`
         res.innerHTML += `<p>A soma de todos os numeros é ${soma}</p>`
         res.innerHTML += `<p>A média de todos os numeros é ${parseInt(media)}</p>`
-        
+
     }
-    
+
 }
+
 function salvarItem() {
     const numero = Number(document.querySelector("#addNumber").value);
-    const objectNum = { numero,  
-       done: false
+    const objectNum = {
+        numero,
+        done: false
     };
-    const valores = JSON.stringify(objectNum)
-    //const local = localStorage.setItem('list', valores)
-    console.log(valores)
-       // return local
+    adicionarItemStorage(objectNum);
+    
+}
+
+function  adicionarItemStorage(objectNum) {
+    const valores = JSON.stringify(objectNum);
+    const local = localStorage.setItem('list', valores);
+    return local;
 
 }
 
+function addItens() {
+    const pegarDoLocal = localStorage.getItem('list');  
+}
 
